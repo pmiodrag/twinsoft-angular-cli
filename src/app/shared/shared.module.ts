2fdcf9bc2ce5388945dcbc8a53219a25e25b59f0
miodrag <mpavkovic@gmail.com>
2016-10-24 14:30:06 +0200

    styling changes

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,17 +5,17 @@ import { RouterModule } from '@angular/router';
 import { MaterialModule } from '@angular/material';
 // common page components
 import { NavbarComponent } from './navbar/index';
+import { FooterComponent } from './footer/index';
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
  */
 
 @NgModule({
-  imports: [CommonModule, RouterModule, MaterialModule.forRoot()],
-  declarations: [ NavbarComponent],
-  exports: [ NavbarComponent,
-    CommonModule, FormsModule, RouterModule],
-     schemas:   [ CUSTOM_ELEMENTS_SCHEMA ],
+    imports: [CommonModule, RouterModule, MaterialModule.forRoot()],
+    declarations: [ NavbarComponent, FooterComponent],
+    exports: [ NavbarComponent, FooterComponent, CommonModule, FormsModule, RouterModule],
+    schemas:   [ CUSTOM_ELEMENTS_SCHEMA ],
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders {
